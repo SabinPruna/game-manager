177dc2eb258f3014ee1f5413ef77b3974a710a54
Sabin Pruna <sabinpruna@outlook.com>
2018-07-17 10:19:26 +0300

    Changed GamesView to use commands instead of look behind.

--- GameManager/ViewModels/GamesViewModel.cs
+++ GameManager/ViewModels/GamesViewModel.cs
@@ -1,50 +1,64 @@
-﻿using GameManager.BussinessLayer;
-using GameManager.Models.Entities;
+﻿using System.Windows.Input;
+using GameManager.BussinessLayer;
+using GameManager.Commands;
+using GameManager.DoorsGame;
 using GameManager.ViewModels.PlayerViewModels;
+using GameManager.Views.Pairs;
 
 namespace GameManager.ViewModels
 {
     public class GamesViewModel : BaseViewModel
     {
         private readonly PlayerManager _playerManager;
-        private Player _player;
         private int _score;
 
-        public LoginViewModel LoginViewModel { get; private set; }
-
         #region Constructors
-
-        public GamesViewModel(Player player)
-        {
-            _player = player;
-            _playerManager = new PlayerManager();
-        }
 
         public GamesViewModel()
         {
             LoginViewModel = new LoginViewModel();
 
             _playerManager = new PlayerManager();
+
+            PairsGameCommand = new RelayCommand(param =>
+            {
+                LevelSelectView levelSelectView = new LevelSelectView();
+                levelSelectView.ShowDialog();
+            });
+
+            DoorsGameCommand = new RelayCommand(param =>
+            {
+                DoorsVIew doorsView = new DoorsVIew();
+                doorsView.ShowDialog();
+            });
+
+            TicTacToeGameCommand = new RelayCommand(param =>
+            {
+                TicTacToeView ticTacToeView = new TicTacToeView();
+                ticTacToeView.ShowDialog();
+            });
+            SnakeGameCommand = new RelayCommand(param =>
+            {
+                SnakeView snakeView = new SnakeView();
+                snakeView.ShowDialog();
+            });
+
+            ShopCommand = new RelayCommand(param =>
+            {
+                ShopWindow shopWindow = new ShopWindow();
+                shopWindow.ShowDialog();
+            });
         }
 
         #endregion
 
         #region  Properties
 
-        public Player Player
-        {
-            get => _player;
-            set
-            {
-                if (Equals(value, _player)) return;
-                _player = value;
-                OnPropertyChanged();
-            }
-        }
+        public LoginViewModel LoginViewModel { get; }
 
         public int Score
         {
-            get => _playerManager.GetPlayerScore(Player?.Id);
+            get => _playerManager.GetPlayerScore(LoginViewModel.Player?.Id);
             set
             {
                 if (value == _score) return;
@@ -52,6 +66,12 @@ namespace GameManager.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        public ICommand PairsGameCommand { get; }
+        public ICommand DoorsGameCommand { get; }
+        public ICommand TicTacToeGameCommand { get; }
+        public ICommand SnakeGameCommand { get; }
+        public ICommand ShopCommand { get; }
 
         #endregion
     }
--- GameManager/Views/GamesView.xaml.cs
+++ GameManager/Views/GamesView.xaml.cs
@@ -1,15 +1,7 @@
 ﻿using System.Windows;
-using GameManager.Models.Entities;
-using GameManager.ViewModels;
-using GameManager.ViewModels.Pairs;
-using GameManager.Views.Pairs;
-using GameManager.DoorsGame;
 
 namespace GameManager.Views
 {
-    /// <summary>
-    ///     Interaction logic for GamesView.xaml
-    /// </summary>
     public partial class GamesView : Window
     {
         #region Constructors
@@ -19,47 +11,6 @@ namespace GameManager.Views
             InitializeComponent();
         }
 
-        public GamesView(Player player)
-        {
-            InitializeComponent();
-            DataContext = new GamesViewModel(player);
-        }
-
         #endregion
-
-        private void BtnPairs_Click(object sender, RoutedEventArgs e)
-        {
-            LevelSelect levelSelect = new LevelSelect(this)
-            {
-                DataContext = new LevelSelectViewModel((DataContext as GamesViewModel)?.Player),
-                Owner = this
-            };
-
-            levelSelect.ShowDialog();
-        }
-
-        private void BtnTicTacToe_Click(object sender, RoutedEventArgs e)
-        {
-            TicTacToeView ticTacToeView = new TicTacToeView();
-            ticTacToeView.Show();
-        }
-
-        private void BtnDoors_Click(object sender, RoutedEventArgs e)
-        {
-            DoorsVIew dg = new DoorsVIew();
-            dg.Show();
-        }
-
-        private void BtnSnake_Click(object sender, RoutedEventArgs e)
-        {
-            SnakeView snakeView = new SnakeView();
-            snakeView.Show();
-        }
-
-        private void Shop_Click(object sender, RoutedEventArgs e)
-        {
-            ShopWindow window = new ShopWindow {Owner = this};
-            window.ShowDialog();
-        }
     }
 }
